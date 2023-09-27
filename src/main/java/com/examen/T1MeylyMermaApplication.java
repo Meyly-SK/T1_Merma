6baaa4ff0039450f669eceda0b9d2a67773d2895
Meyly-SK <meylymerma03@gmail.com>
2023-09-26 20:05:45 -0500

    Primer Commit

---
+++ src/main/java/com/examen/T1MeylyMermaApplication.java
@@ -0,0 +1,13 @@
+package com.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1MeylyMermaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1MeylyMermaApplication.class, args);
+	}
+
+}
